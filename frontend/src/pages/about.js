84f150b9f3a44bb2c53c8394a0d636c9adba90eb
Ceyyy123 <SIM210485@spengergasse.at>
2024-11-06 17:44:33 +0100

    commit next-server changes

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Navbar from './navbar'; // Korrigierter Pfad zur Navbar-Komponente
+import Navbar from './navbar'
 import Footer from './footer'; // Korrigierter Pfad zur Footer-Komponente
 import styles from '../styles/About.module.css'; // Stile für die About-Seite
 
 function About() {
   return (
     <div className={styles.container}>
-      <Navbar /> {/* Navbar-Komponente wird hier verwendet */}
+      <Navbar />
       <div className={styles.content}>
         <h2 className={styles.heading}>Über Uns</h2>
         <p className={styles.paragraph}>HTL Spengergasse</p>
       </div>
-      <Footer /> {/* Footer-Komponente wird hier verwendet */}
+      <Footer />
     </div>
   );
 }
