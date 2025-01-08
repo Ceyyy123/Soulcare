b8a27c90ce343438c102f001060a6114b6aedc47
ceyda simsek <SIM210485@spengergasse.at>
2025-01-08 20:03:09 +0100

    import express

--- frontend/api/journal.js
+++ frontend/api/journal.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from '../../node_modules/express';
 import { connectToDatabase } from '../../lib/mongodb'; // MongoDB-Verbindung
 const router = express.Router();
 
